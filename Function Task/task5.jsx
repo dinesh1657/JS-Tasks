2d777572ca97336a846b47fa012b6bc5d698b899
Dinesh <dineshsivakumar1657@gmail.com>
2026-09-01 16:07:24 +0530

    new

--- Function Task/task5.jsx
+++ Function Task/task5.jsx
@@ -2,36 +2,45 @@
 
 const fruits = ["Apple", "Banana", "Mango"];
 
-const [fruits1,fruits2,fruits3] = fruits;
+const [fruits1, fruits2, fruits3] = fruits;
 
 console.log(fruits1);
 console.log(fruits2);
-console.log(fruits3); 
+console.log(fruits3);
 
 //Remove mid element from array using destructuring
 
 const fruit = ["Apple", "Banana", "Mango"];
 
-const [fruit1, ,fruit3] = fruit;
+const [fruit1, , fruit3] = fruit;
 
 console.log(fruit1);
 console.log(fruit3);
 
 //Default values in destructuring
 
-const number = [10,20];
-const [num1,num2,num3=30] = number;
+const number = [10, 20];
+const [num1, num2, num3 = 30] = number;
 
 console.log(num1);
 console.log(num2);
-console.log(num3);  
+console.log(num3);
 
 //Rest with Array Destructuring
 
-const numbers = [10,20,30,40,50];
+const numbers = [10, 20, 30, 40, 50];
 
-const [numb1,numb2,...rest] = numbers;
+const [numb1, numb2, ...rest] = numbers;
 
 console.log(numb1);
 console.log(numb2);
 console.log(rest);
+
+//Rest with Array
+
+const numbers1 = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+const [number1, number2, ...remaining] = numbers1;
+
+console.log(number1);
+console.log(number2);
+console.log(remaining);
