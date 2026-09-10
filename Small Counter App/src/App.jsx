ba2a3e90650cdcb980b6d475baf3160f97964397
Dinesh <dineshsivakumar1657@gmail.com>
2026-09-10 17:04:01 +0530

    state,effect

--- Small Counter App/src/App.jsx
+++ Small Counter App/src/App.jsx
@@ -1,29 +1,49 @@
-import react from "react";
+import React, { useState, useEffect } from "react";
 
-function addItem(Counter, setCounter) {
-  setCounter(Counter + 1);
+function addItem(counter, setCounter) {
+  setCounter(counter + 1);
 }
 
-function removeItem(Counter, setCounter) {
-  setCounter(Counter - 1);
+function removeItem(counter, setCounter) {
+  setCounter(counter - 1);
 }
 
 export default function App() {
-  const [Counter, setCounter] = react.useState(0);
+  const [counter, setCounter] = useState(0);
+  useEffect(() => {
+    console.log("Counter changed:", counter);
+
+    document.title = `iPhone - ${counter} items`;
+  }, [counter]);
 
   return (
     <div className="App">
-      <img src="https://media.karousell.com/media/photos/products/2022/10/28/apple_iphone_14_pro_max_512gb__1666943808_3e7b42f0.jpg" alt="Apple Logo" style={{ width: "100px", height: "100px" }} />
-      <h1>Iphone 14 Plus</h1>
-      <p>Price: 69,999 Rs</p>
-      <h2>{Counter}</h2>
-      <button onClick={() => addItem(Counter, setCounter)}>Add</button>
-      <button onClick={() => removeItem(Counter, setCounter)} disabled={Counter === 0}>
+
+      <img
+        src="https://media.karousell.com/media/photos/products/2022/10/28/apple_iphone_14_pro_max_512gb__1666943808_3e7b42f0.jpg"
+        alt="iPhone"
+        style={{
+          width: "100px",
+          height: "100px"
+        }}
+      />
+      <h1>iPhone 17</h1>
+      <p>Price: ₹99,999</p>
+      <h2>Quantity: {counter}</h2>
+
+      <button
+        onClick={() => addItem(counter, setCounter)}
+      >
+        Add
+      </button>
+
+      <button
+        onClick={() => removeItem(counter, setCounter)}
+        disabled={counter === 0}
+      >
         Remove
       </button>
+
     </div>
   );
 }
-
-
-//
