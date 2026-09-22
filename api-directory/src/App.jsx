98815da32ed1c27148d7579e8ac2af0eb32baf45
Dinesh <dineshsivakumar1657@gmail.com>
2026-09-22 11:40:26 +0530

    Edit

--- api-directory/src/App.jsx
+++ api-directory/src/App.jsx
@@ -2,55 +2,59 @@ import { useEffect, useState } from "react";
 
 function App() {
   const [users, setUsers] = useState([]);
-  const [search, setSearch] = useState("");
+  const [searchText, setSearchText] = useState("");
   const [loading, setLoading] = useState(true);
 
-  // Fetch users when component loads
+  // Fetch users
   useEffect(() => {
-    fetch("https://jsonplaceholder.typicode.com/users")
-      .then((response) => response.json())
-      .then((data) => {
+    const getUsers = async () => {
+      try {
+        const response = await fetch(
+          "https://jsonplaceholder.typicode.com/users"
+        );
+
+        const data = await response.json();
+
         setUsers(data);
+      } catch (error) {
+        console.log("Error:", error);
+      } finally {
         setLoading(false);
-      })
-      .catch((error) => {
-        console.log(error);
-        setLoading(false);
-      });
+      }
+    };
+
+    getUsers();
   }, []);
 
-  // Search users by name
-  const filteredUsers = users.filter((user) =>
-    user.name.toLowerCase().includes(search.toLowerCase())
+  // Filter users
+  const searchResults = users.filter((user) =>
+    user.name.toLowerCase().includes(searchText.toLowerCase())
   );
 
   return (
     <div>
-      <h1>API User Directory</h1>
+      <h1>User Directory</h1>
 
       <input
         type="text"
-        placeholder="Search user by name..."
-        value={search}
-        onChange={(e) => setSearch(e.target.value)}
+        placeholder="Search user..."
+        value={searchText}
+        onChange={(e) => setSearchText(e.target.value)}
       />
 
       {loading ? (
-        <p>Loading...</p>
+        <h3>Loading users...</h3>
       ) : (
         <div>
-          {filteredUsers.length > 0 ? (
-            filteredUsers.map((user) => (
-              <div key={user.id}>
-                <h3>{user.name}</h3>
-                <p>Email: {user.email}</p>
-                <p>Username: {user.username}</p>
-                <hr />
-              </div>
-            ))
-          ) : (
-            <p>No users found</p>
-          )}
+          {searchResults.map((user) => (
+            <div key={user.id}>
+              <h2>{user.name}</h2>
+              <p>Email: {user.email}</p>
+              <p>Phone: {user.phone}</p>
+              <p>Website: {user.website}</p>
+              <hr />
+            </div>
+          ))}
         </div>
       )}
     </div>
